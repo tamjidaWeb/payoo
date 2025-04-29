b4388cb8f4e5abda7bbc86da04ae8b4db9a6a29c
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-04-29 16:59:03 +0600

    c

--- home.js
+++ home.js
@@ -14,5 +14,27 @@ document.getElementById('btn-add-money').addEventListener('click',function(e){
     //get the pin number
 
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput)
+    console.log(pinNumberInput);
+
+
+    // step 3: verify the pin number
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+
+        //step 4: get the corrent balance
+        const balance = document.getElementById('current-balance').innerText;
+        console.log(balance);
+
+        //step 5: addMoneyInput with balance
+        const newBalance = parseFloat(balance) + parseFloat(addMoneyInput);
+        console.log(newBalance);
+
+
+        //step 6:update the balance in the UI/DOM
+        document.getElementById('current-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('please try again!')
+    }
 })
