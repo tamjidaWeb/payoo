8092eee69417ef66432e9c97efdc6b7d104b0890
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-04-29 15:35:48 +0600

    c

--- payoo.js
+++ payoo.js
@@ -1,12 +1,47 @@
-// step 1: set event handler
-document.getElementById('btn-login').addEventListener('click', function(event){
-    //step 2: prevent default behavior reloading browser
-    event.preventDefault();
-    console.log('login button clicked');
-    // step 3: get the phone number
+// // step 1: set event handler
+// document.getElementById('btn-login').addEventListener('click', function(event){
+//     //step 2: prevent default behavior reloading browser
+//     event.preventDefault();
+  
+//     // step 3: get the phone number and the pin number
 
-    const phoneNumber = document.getElementById('phone-number');
-    const phonNumberValue = phoneNumber.value;
-    console.log(phonNumberValue);
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+
+//     console.log(phoneNumber,pinNumber);
+
+//     // step 4:validate phone and pin number
+//     if(pinNumber ==='1234' && phoneNumber.startsWith('01') && phoneNumber.length === 3){
+//         console.log('you are logged in successfully')
+
+//         // step 5: allow user to use the website
+//     }
+//     else{
+//         alert('wrong pin or pin number')
+//     }
     
-})
+// })
+
+
+
+
+document.getElementById('btn-login').
+        addEventListener('click', function(event){
+        event.preventDefault();
+
+
+        // get phone number and pin
+
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber,pinNumber);
+
+        if(pinNumber === '1234' && phoneNumber.startsWith('01') && phoneNumber.length === 3){
+            console.log('you are logged in successfully');
+            window.location.href = '/home.html';
+        }
+            else{
+                alert('wrong pin or pin number');
+            }
+
+               })
