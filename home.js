c04e83fb11e8f5669e0aaef8c2cdd96ba9599fc6
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-04-29 16:18:17 +0600

    added

--- home.js
+++ home.js
@@ -1,1 +1,18 @@
-console.log('added')
+//add money to the account 
+
+//step 1: add an event handler to the add money button inside the form
+
+document.getElementById('btn-add-money').addEventListener('click',function(e){
+    //prevent page reload after form submit
+    e.preventDefault();
+    console.log('money add bttn clicked');
+
+    //step 2: get the value of the input field
+    const addMoneyInput = document.getElementById('amount-input').value;
+    console.log(addMoneyInput);
+
+    //get the pin number
+
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+})
